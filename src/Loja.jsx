4a91b9b5e17521d7a5602bc4efd565e28c081d22
Vitor Chaves <vitor.chaves@iesb.edu.br>
2023-06-25 15:59:51 -0300

    integração dos produtos ao database #9

--- src/Loja.jsx
+++ src/Loja.jsx
@@ -1,27 +1,33 @@
 import React, { useState, useEffect } from "react";
-import produtos from './produtos.json';
 import { Link } from "react-router-dom";
+import { db } from "./firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
 
 const Loja = () => {
-    const [items, setItems] = useState([]);
+    const [produtos, setProdutos] = useState([]);
 
     useEffect(() => {
-        setItems(produtos);
-    }, []);
+        const fetchProdutos = async () => {
+            const querySnapshot = await getDocs(collection(db, "products"));
+            const fetchedProdutos = querySnapshot.docs.map((doc) => doc.data());
+            setProdutos(fetchedProdutos);
+        };
 
+        fetchProdutos();
+    }, []);
     return (
         <>
             <div className="container-fluid bg-secondary">
                 <h1>Produtos</h1>
                 <div className="row row-cols-3 g-3">
-                    {items.map((produto) => (
+                    {produtos.map((produto) => (
                         <div className="col" key={produto.id}>
                             <div className="card h-100">
                                 <img src={produto.imagem} className="card-img-top imagemCard" alt="produto" />
                                 <div className="card-body">
                                     <h5 className="card-title">{produto.nome}</h5>
                                     <p className="card-text">R${produto.preco}</p>
-                                    <Link className="btn btn-success" to={`/produto/${produto.id}`}>Abrir Produto</Link>
+                                    <Link className="btn btn-success" to={{pathname: `/produto/${produto.id}`}}>Abrir Produto</Link>
                                 </div>
                             </div>
                         </div>
--- src/Produto.jsx
+++ src/Produto.jsx
@@ -1,28 +1,57 @@
-import React, {useState} from "react";
-import produtos from './produtos.json';
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./firebaseConfig";
 
-const Produto = ({adicionar}) => {
+const Produto = ({ adicionar }) => {
     const { id } = useParams();
-    let p = produtos[id - 1];
+    const [produto, setProduto] = useState(null);
+
+    useEffect(() => {
+        const fetchDetalhesProdutos = async () => {
+            try {
+                const docRef = doc(db, "products", id);
+                const docSnapshot = await getDoc(docRef);
+
+                if (docSnapshot.exists()) {
+                    const data = docSnapshot.data();
+                    setProduto({ id: docSnapshot.id, ...data });
+                } else {
+                    console.log("Produto não encontrado");
+                }
+            } catch (error) {
+                console.log("Erro ao obter os detalhes do produto", error);
+            }
+        };
+
+        fetchDetalhesProdutos();
+    }, [id]);
+
+    if (!produto) {
+        return <div className="container-fluid bg-secondary carregandoProduto">
+            <p>
+                Carregando detalhes do produto...
+            </p>
+        </div>;
+    }
     return (
         <>
             <div className="container-fluid bg-secondary containerProduto">
                 <div className="container bg-white">
                     <div className="row gx-4 gx-lg-5 align-items-center">
-                        <div className="col-md-6"><img className="card-img-top mb-5 mb-md-0 imagemProduto" src={p.imagem} alt="..." /></div>
+                        <div className="col-md-6"><img className="card-img-top mb-5 mb-md-0 imagemProduto" src={produto.imagem} alt="..." /></div>
                         <div className="col-md-6">
-                            <div className="small mb-1">{p.categoria}</div>
-                            <h1 className="display-5 fw-bolder">{p.nome}</h1>
+                            <div className="small mb-1">{produto.categoria}</div>
+                            <h1 className="display-5 fw-bolder">{produto.nome}</h1>
                             <div className="fs-5 mb-5">
-                                <span>R${p.preco}</span>
+                                <span>R${produto.preco}</span>
                             </div>
-                            <p className="">{p.descricao}</p>
+                            <p className="">{produto.descricao}</p>
 
                             <div className="small mb-1 mt-5">Quantidade</div>
                             <div className="d-grid gap-2">
                                 <input className="form-control text-center mb-1" id="quantidadeProduto" type="num" defaultValue="1" />
-                                <button className="btn btn-success flex-shrink-0" type="button" onClick={() => adicionar(p)}>Adicionar ao Carrinho</button>
+                                <button className="btn btn-success flex-shrink-0" type="button" onClick={() => adicionar(produto)}>Adicionar ao Carrinho</button>
                             </div>
                         </div>
                     </div>
