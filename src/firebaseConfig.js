1b41dd8fc20bc06f2c69e7b652806941440327d4
Vitor Chaves <vitor.chaves@iesb.edu.br>
2023-06-25 13:58:19 -0300

    adicionando conexão a base de dados firestorm cloud #9

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyC-cHEldnYvzaRRQWALmTqzrKBCG-oXcIk",
@@ -11,3 +12,4 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
