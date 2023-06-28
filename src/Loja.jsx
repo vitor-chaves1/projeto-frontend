bd262aff3a5efc70c8a9d988265c21038d165cce
Vitor Chaves <vitor.chaves@iesb.edu.br>
2023-06-28 19:02:43 -0300

    ajuste no tamanho de imagens e colunas #19

--- src/Loja.jsx
+++ src/Loja.jsx
@@ -19,11 +19,11 @@ const Loja = () => {
         <>
             <div className="container-fluid bg-secondary">
                 <h1>Produtos</h1>
-                <div className="row row-cols-3 g-3">
+                <div className="row g-3">
                     {produtos.map((produto) => (
-                        <div className="col" key={produto.id}>
+                        <div className="col-md-4" key={produto.id}>
                             <div className="card h-100">
-                                <img src={produto.imagem} className="card-img-top imagemCard" alt="produto" />
+                                <img src={produto.imagem} className="card-img-top img-fluid imagemCard" alt="produto" />
                                 <div className="card-body">
                                     <h5 className="card-title">{produto.nome}</h5>
                                     <p className="card-text">R${produto.preco}</p>
