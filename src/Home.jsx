63657f0b9166ace2d1bd1a5d0b5805c555b115c9
Vitor Chaves <vitor.chaves@iesb.edu.br>
2023-06-28 17:29:59 -0300

    Pagina home refeita #18

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,83 +1,47 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
     render() {
         return (
             <>
-                <div className="container menuprincipal">
-                    <h1 className="p-2 ">
-                        Bem vindo a Projeto Loja
-                    </h1>
-                    <div id="carouselExample" class="carousel slide m-1 p-1">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <div class="card" aria-hidden="true">
-                                    <img src="..." class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title placeholder-glow">
-                                            <span class="placeholder col-6"></span>
-                                        </h5>
-                                        <p class="card-text placeholder-glow">
-                                            <span class="placeholder col-7"></span>
-                                            <span class="placeholder col-4"></span>
-                                            <span class="placeholder col-4"></span>
-                                            <span class="placeholder col-6"></span>
-                                            <span class="placeholder col-8"></span>
-                                        </p>
-                                        <a class="btn btn-primary disabled placeholder col-6"></a>
-                                    </div>
-                                </div>
+                <main>
+                    <div className="container p-4 bg-body-tertiary pb-2">
+                        <div className="shadow-lg p-3 mb-5 rounded pb-4">
+                            <h1 className="display-3">Bem vindo a Projeto-Loja</h1>
+                            <p className="fs-4 p-4">
+                                Aqui na Projeto-loja, você pode abrir o catálogo de produtos, adicionar produtos ao carrinho, deletar produtos do carrinho, verificar o carrinho, e finalizar uma compra. Registre-se já para fazer suas compras!
+                            </p>
+                            <Link className="btn btn-primary ms-4" to={"/loja"}>Acessar Loja »</Link>
+                        </div>
+                        <div className="row p-4">
+                            <div className="col-md-4">
+                                <h2>Firebase</h2>
+                                <p>
+                                    O webapp possui serviços da Firebase Authentication (Google) para registrar e logar o usuário, e Cloud Firestore (Google) para as funcionalidades CRUD.
+                                </p>
                             </div>
-                            <div class="carousel-item">
-                                <div class="card" aria-hidden="true">
-                                    <img src="..." class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title placeholder-glow">
-                                            <span class="placeholder col-6"></span>
-                                        </h5>
-                                        <p class="card-text placeholder-glow">
-                                            <span class="placeholder col-7"></span>
-                                            <span class="placeholder col-4"></span>
-                                            <span class="placeholder col-4"></span>
-                                            <span class="placeholder col-6"></span>
-                                            <span class="placeholder col-8"></span>
-                                        </p>
-                                        <a class="btn btn-primary disabled placeholder col-6"></a>
-                                    </div>
-                                </div>
+                            <div className="col-md-4">
+                                <h2>React + Bootstrap</h2>
+                                <p>
+                                    Projeto-Loja foi feita usando a biblioteca React para a criação e navegação entre as interfaces de usuário. O estilo foi feito usando classes do framework Bootstrap, para deixar um visual bonito e uniforme para todos os tipos de telas.
+                                </p>
                             </div>
-                            <div class="carousel-item">
-                                <div class="card" aria-hidden="true">
-                                    <img src="..." class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title placeholder-glow">
-                                            <span class="placeholder col-6"></span>
-                                        </h5>
-                                        <p class="card-text placeholder-glow">
-                                            <span class="placeholder col-7"></span>
-                                            <span class="placeholder col-4"></span>
-                                            <span class="placeholder col-4"></span>
-                                            <span class="placeholder col-6"></span>
-                                            <span class="placeholder col-8"></span>
-                                        </p>
-                                        <a class="btn btn-primary disabled placeholder col-6"></a>
-                                    </div>
-                                </div>
+                            <div className="col-md-4">
+                                <h2>Produtos</h2>
+                                <p>
+                                    Os produtos foram puxados do API 'fakestoreapi' para deixar a loja com cara de loja de verdade. <a className="link-secondary" target="_blank" rel="noreferrer" href="https://fakestoreapi.com/">Clique aqui para saber mais</a>
+                                </p>
                             </div>
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
+                        <hr />
+                        <p>
+                            @ Vitor Chaves Araujo - IESB - Análise e Desenvolvimento de Sistemas - Projeto Front-End - <a target="_blank" rel="noreferrer" href="https://github.com/vitoriesb1/projeto-frontend">Github</a>
+                        </p>
+                        
                     </div>
-                    <div class="d-grid m-1 p-1">
-                        <button class="btn btn-secondary" type="button">Acessar Loja</button>
-                    </div>
-                </div>
+
+                </main>
             </>
         )
     }
